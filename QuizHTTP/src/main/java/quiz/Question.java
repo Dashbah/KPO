bac94f3d3523fd2ca31e6b87976741df16b38e13
Darya Bakhankova <dsbakhankova@edu.hse.ru>
2023-06-23 21:10:15 +0300

    Update Question.java

--- QuizHTTP/src/main/java/quiz/Question.java
+++ QuizHTTP/src/main/java/quiz/Question.java
@@ -12,35 +12,6 @@ public class Question {
     private String answer;
     @SerializedName("question")
     private String question;
-//    @SerializedName("value")
-//    private int value;
-//    @SerializedName("airdate")
-//    private String airdate;
-//    @SerializedName("created_at")
-//    private String createdAt;
-//    @SerializedName("updated_at")
-//    private String updatedAt;
-//    @SerializedName("category_id")
-//    private int categoryId;
-//    @SerializedName("game_id")
-//    private int gameId;
-//    @SerializedName("invalid_count")
-//    private int invalidCount;
-//    @SerializedName("category")
-//    private Category category;
-
-//    private class Category {
-//        @SerializedName("id")
-//        int id;
-//        @SerializedName("title")
-//        String title;
-//        @SerializedName("created_at")
-//        String createdAt;
-//        @SerializedName("updated_at")
-//        String updatedAt;
-//        @SerializedName("clues_count")
-//        int cluesCount;
-//    }
 
     String getAnswer() {
         return answer;
